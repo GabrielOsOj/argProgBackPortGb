0a092c69708ff964d9267851db351f3dfce1071e
GabrielOsOj <cuentaauxiliar2@hotmail.com>
2022-10-13 17:26:58 -0300

    creacion de proyecto springboot
    en este primer commint, cree el proyecto de springboot y sus dependencias

---
+++ src/main/java/com/argentinaPrograma/argProgApi/ArgProgApiApplication.java
@@ -0,0 +1,13 @@
+package com.argentinaPrograma.argProgApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgProgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgProgApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argentinaPrograma/argProgApi/ArgProgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argentinaPrograma.argProgApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgProgApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
