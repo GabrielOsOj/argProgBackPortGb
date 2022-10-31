bf92bd719101fe411bd506d7e3af859c0177abdc
GabrielOsOj <cuentaauxiliar2@hotmail.com>
2022-10-31 16:48:45 -0300

    Update usuariosControlador.java

--- src/main/java/com/argentinaPrograma/argProgApi/seguridad/controlador/usuariosControlador.java
+++ src/main/java/com/argentinaPrograma/argProgApi/seguridad/controlador/usuariosControlador.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "**")
+@CrossOrigin("**")
 public class usuariosControlador {
 
     @Autowired
